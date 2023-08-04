d70b22a6bee88f6a78e25d98adf02d25433e1a47
Yassin-Aru <jocarhisouka@gmail.com>
2023-08-04 15:01:47 +0100

    Fix Restaurant Details css

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -55,7 +55,7 @@ const RestaurantDetails = () => {
             </div>
             {/* RATING */} {/* DESCRIPTION */}
             <div className="mt-4">
-              <p className="text-lg font-light">
+              <p className="text-lg ">
                 The classics you love prepared with a perfect twist, all served
                 up in an atmosphere that feels just right. That’s the Milestones
                 promise. So, whether you’re celebrating a milestone, making the
@@ -117,7 +117,7 @@ const RestaurantDetails = () => {
                         <div className="flex mr-5">*****</div>
                       </div>
                       <div className="mt-5">
-                        <p className="text-lg font-light">
+                        <p className="text-lg ">
                           Laurie was on top of everything! Slow night due to the
                           snow storm so it worked in our favor to have more one
                           on one with the staff. Delicious and well worth the
@@ -132,14 +132,14 @@ const RestaurantDetails = () => {
             </div>
             {/* REVIEWS */}
           </div>
-          <div className="w-[27%] relative text-reg">
-            <div className="fixed w-[15%] bg-white rounded p-3 shadow">
+          <div className="w-[27%] relative text-reg pb-8">
+            <div className="w-[100%] bg-white rounded p-3 shadow">
               <div className="text-center border-b pb-2 font-bold">
                 <h4 className="mr-7 text-lg">Make a Reservation</h4>
               </div>
               <div className="my-3 flex flex-col">
                 <label htmlFor="">Party size</label>
-                <select name="" className="py-3 border-b font-light" id="">
+                <select name="" className="py-2 border-b  bg-white" id="">
                   <option value="">1 person</option>
                   <option value="">2 people</option>
                 </select>
@@ -149,19 +149,19 @@ const RestaurantDetails = () => {
                   <label htmlFor="">Date</label>
                   <input
                     type="text"
-                    className="py-3 border-b font-light w-28"
+                    className="py-2 border-2 rounded border-black  w-28 h-[40px] bg-white"
                   />
                 </div>
                 <div className="flex flex-col w-[48%]">
                   <label htmlFor="">Time</label>
-                  <select name="" id="" className="py-3 border-b font-light">
+                  <select name="" id="" className="py-2 h-[40px] border-2 rounded bg-white border-black">
                     <option value="">7:30 AM</option>
                     <option value="">9:30 AM</option>
                   </select>
                 </div>
               </div>
               <div className="mt-5">
-                <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">
+                <button className="bg-red-600 rounded w-full px-2 text-white font-bold h-10">
                   Find a Time
                 </button>
               </div>
