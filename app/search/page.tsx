59395064c086f198469375c9eeaecb1272497cb1
Yassin-Aru <jocarhisouka@gmail.com>
2023-08-05 13:18:35 +0100

    Create&Setup Search Header Comp...

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import Navbar from "../components/Navbar";
+import Header from "./components/Header";
 
 const Search = () => {
   return (
     <main className="bg-gray-100 min-h-screen w-full">
       <main className="max-w-screen-2xl m-auto bg-white">
         <Navbar />
-        {/* HEADER */}
-        
+        {/* SEARCH HEADER */}
+        <Header />
         <div className="flex py-4 m-auto w-2/3 justify-between items-start text-black">
           {/* SEARCH SIDE BAR */}
           <div className="w-1/5">
