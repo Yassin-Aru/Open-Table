9230abb0e350375d7923cb0304518091b30a2f63
Yassin-Aru <jocarhisouka@gmail.com>
2023-08-05 13:20:24 +0100

    Create&Setup Search SideBar Comp...

---
+++ app/search/components/SideBar.tsx
@@ -0,0 +1,36 @@
+
+function SideBar() {
+  return (
+    <div className="w-1/5">
+      <div className="border-b pb-4">
+        <h1 className="mb-2">Region</h1>
+        <p className="uppercase text-reg">Toronto</p>
+        <p className="uppercase text-reg">Ottawa</p>
+        <p className="uppercase text-reg">Montreal</p>
+        <p className="uppercase text-reg">Hamilton</p>
+        <p className="uppercase text-reg">Kingston</p>
+        <p className="uppercase text-reg">Niagara</p>
+      </div>
+      <div className="border-b pb-4 mt-3">
+        <h1 className="mb-2">Cuisine</h1>
+        <p className="uppercase text-reg">Italian</p>
+        <p className="uppercase text-reg">Mexican</p>
+        <p className="uppercase text-reg">Chinese</p>
+      </div>
+      <div className="mt-3 pb-4">
+        <h1 className="mb-2">Price</h1>
+        <div className="flex">
+          <button className="border w-full text-reg  rounded-l p-2">$</button>
+          <button className="border-r border-t border-b w-full text-reg  p-2">
+            $$
+          </button>
+          <button className="border-r border-t border-b w-full text-reg  p-2 rounded-r">
+            $$$
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 import Header from "./components/Header";
+import SideBar from "./components/SideBar";
 
 const Search = () => {
   return (
@@ -10,39 +11,7 @@ const Search = () => {
         {/* SEARCH HEADER */}
         <Header />
         <div className="flex py-4 m-auto w-2/3 justify-between items-start text-black">
-          {/* SEARCH SIDE BAR */}
-          <div className="w-1/5">
-            <div className="border-b pb-4">
-              <h1 className="mb-2">Region</h1>
-              <p className="uppercase text-reg">Toronto</p>
-              <p className="uppercase text-reg">Ottawa</p>
-              <p className="uppercase text-reg">Montreal</p>
-              <p className="uppercase text-reg">Hamilton</p>
-              <p className="uppercase text-reg">Kingston</p>
-              <p className="uppercase text-reg">Niagara</p>
-            </div>
-            <div className="border-b pb-4 mt-3">
-              <h1 className="mb-2">Cuisine</h1>
-              <p className="uppercase text-reg">Italian</p>
-              <p className="uppercase text-reg">Mexican</p>
-              <p className="uppercase text-reg">Chinese</p>
-            </div>
-            <div className="mt-3 pb-4">
-              <h1 className="mb-2">Price</h1>
-              <div className="flex">
-                <button className="border w-full text-reg  rounded-l p-2">
-                  $
-                </button>
-                <button className="border-r border-t border-b w-full text-reg  p-2">
-                  $$
-                </button>
-                <button className="border-r border-t border-b w-full text-reg  p-2 rounded-r">
-                  $$$
-                </button>
-              </div>
-            </div>
-          </div>
-          {/* SEARCH SIDE BAR */}
+          <SideBar />
           <div className="w-5/6">
             {/* RESAURANT CAR */}
             <div className="border-b flex pb-5">
