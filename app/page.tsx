bf1399d954f00a2bdedd8346e4f3a51d8ced6603
Yassin-Aru <jocarhisouka@gmail.com>
2023-08-05 12:31:46 +0100

    Adding Link Route to Restaurant Card.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 import Link from "next/link";
 
-
 export default function Home() {
   return (
     <main className="bg-gray-100  w-full">
       <main className="max-w-full-2xl m-auto bg-white">
         {/* NAVBAR */}
         <nav className="bg-white p-2 flex justify-between">
-          <Link href="/" className="font-bold text-gray-700 text-2xl"> OpenTable </Link>
+          <Link href="/" className="font-bold text-gray-700 text-2xl">
+            {" "}
+            OpenTable{" "}
+          </Link>
           <div>
             <div className="flex">
-              <button
-                className="bg-blue-400 text-white border p-1 px-4 rounded mr-3 font-bold"
-              >
+              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3 font-bold">
                 Sign in
               </button>
-              <button className="border p-1 px-4 rounded text-blue-400 font-bold">Sign up</button>
+              <button className="border p-1 px-4 rounded text-blue-400 font-bold">
+                Sign up
+              </button>
             </div>
           </div>
         </nav>
@@ -44,27 +46,27 @@ export default function Home() {
           {/* HEADER */} {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
             {/* CARD */}
-            <div
-              className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer"
-            >
-              <img
-                src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
-                alt=""
-                className="w-full h-36"
-              />
-              <div className="p-1 text-black">
-                <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
-                <div className="flex items-start">
-                  <div className="flex mb-2">*****</div>
-                  <p className="ml-2">77 reviews</p>
+            <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
+              <Link href="/restaurant/ivarus">
+                <img
+                  src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
+                  alt=""
+                  className="w-full h-36"
+                />
+                <div className="p-1 text-black">
+                  <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
+                  <div className="flex items-start">
+                    <div className="flex mb-2">*****</div>
+                    <p className="ml-2">77 reviews</p>
+                  </div>
+                  <div className="flex text-reg font-light capitalize">
+                    <p className=" mr-3">Mexican</p>
+                    <p className="mr-3">$$$$</p>
+                    <p>Toronto</p>
+                  </div>
+                  <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
                 </div>
-                <div className="flex text-reg font-light capitalize">
-                  <p className=" mr-3">Mexican</p>
-                  <p className="mr-3">$$$$</p>
-                  <p>Toronto</p>
-                </div>
-                <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
-              </div>
+              </Link>
             </div>
             {/* CARD */}
           </div>
@@ -72,5 +74,5 @@ export default function Home() {
         </main>
       </main>
     </main>
-  )
+  );
 }
