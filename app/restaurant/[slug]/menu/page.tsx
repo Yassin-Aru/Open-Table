021443b0b38fd9d090788153033bec8ba10f4050
Yassin-Aru <jocarhisouka@gmail.com>
2023-08-05 12:36:29 +0100

    Setting the basics Links in /restaurant/.../Menu

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -31,14 +31,12 @@ const RestaurantMenu = () => {
           <div className="bg-white w-[100%] rounded p-3 shadow">
             {/* RESAURANT NAVBAR */}
             <nav className="flex text-reg border-b pb-2">
-              <a href="" className="mr-7">
-                {" "}
-                Overview{" "}
-              </a>
-              <a href="" className="mr-7">
-                {" "}
-                Menu{" "}
-              </a>
+              <Link href="/restaurant/ivarus" className="mr-7">
+                Overview
+              </Link>
+              <Link href="/restaurant/ivarus/menu" className="mr-7">
+                Menu
+              </Link>
             </nav>
             {/* RESAURANT NAVBAR */} {/* MENU */}
             <main className="bg-white mt-5">
