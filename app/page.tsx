eb47d7bb162b6a94262b5859344a75f26522d4ff
Yassin-Aru <jocarhisouka@gmail.com>
2023-08-09 11:10:18 +0100

    Fetching the Restaurant Table data to the sever side

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,17 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const fetchRestaurant = async () => {
+  const restauratns = await prisma.restaurant.findMany();
+  return restauratns;
+};
 
 export default async function Home() {
+  const restauratns = await fetchRestaurant();
+  
+  console.log({restauratns});
+  
+
   return (
     <main>
       <Header />
