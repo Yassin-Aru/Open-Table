5bbe5fd6f76e59e0bb88c492128544a9d20f0e9b
Yassin-Aru <jocarhisouka@gmail.com>
2023-08-09 15:52:02 +0100

    Types of Resto Details Comp ...

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -7,34 +7,55 @@ import RestaurantNavBar from "./components/RestaurantNavBar";
 import Reviews from "./components/Reviews";
 import Title from "./components/Title";
 
-const prisma = new PrismaClient();
-
-const fetchRestaurantBySlug = async (slug: string) => {
-  const restaurant = prisma.restaurant.findUnique({
-    where: {
-      slug
-    }
-  })
+interface Restaurant {
+  id: number;
+  name: string;
+  description: string;
+  images: string[];
+  slug: string;
 }
 
-const RestaurantDetails = async (props: any) => {
-  console.log({props});
-  
-  // const restaurant = await fetchRestaurantBySlug()
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant;
+};
+
+const RestaurantDetails = async ({ params }: { params: { slug: string } }) => {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+  console.log(restaurant);
+
   return (
     <>
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          <RestaurantNavBar />
-          <Title />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-          {/* REVIEWS */}
-        </div>
-        <div className="w-[27%] relative text-reg pb-8">
-          <ReservationCard />
-        </div>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+        {/* REVIEWS */}
+      </div>
+      <div className="w-[27%] relative text-reg pb-8">
+        <ReservationCard />
+      </div>
     </>
   );
 };
