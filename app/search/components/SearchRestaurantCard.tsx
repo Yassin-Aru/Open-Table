0d3fd6713c1e8c1ff5dd19f791755eb93c0a86a0
Yassin-Aru <jocarhisouka@gmail.com>
2023-08-09 23:44:40 +0100

    Fetching Data for Search op

--- app/search/components/SearchRestaurantCard.tsx
+++ app/search/components/SearchRestaurantCard.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
+import { RestaurantCardType } from "../../page";
 
-function SearchRestaurantCard({city}: {city: string}) {
-  console.log({city});
+interface Props {
+  restaurant: RestaurantCardType;
+}
+
+function SearchRestaurantCard({restaurant}: Props) {
   
   return (
     <div className="border-b flex pb-5">
       <img
-        src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-        alt=""
+        src={restaurant.main_image}
+        alt={restaurant.name}
         className="w-44 rounded"
       />
       <div className="pl-5">
-        <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+        <h2 className="text-3xl">{restaurant.name}</h2>
         <div className="flex items-start">
           <div className="flex mb-2">*****</div>
           <p className="ml-2 text-sm">Awesome</p>
@@ -19,8 +23,8 @@ function SearchRestaurantCard({city}: {city: string}) {
         <div className="mb-9">
           <div className=" flex text-reg">
             <p className="mr-4">$$$</p>
-            <p className="mr-4">Mexican</p>
-            <p className="mr-4">Ottawa</p>
+            <p className="mr-4">{restaurant.cuisine.name}</p>
+            <p className="mr-4">{restaurant.location.name}</p>
           </div>
         </div>
         <div className="text-red-600">
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,35 @@
+import { PrismaClient } from "@prisma/client";
 import Header from "./components/Header";
 import SearchRestaurantCard from "./components/SearchRestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
 
-const Search = ({searchParams}: {searchParams: {city: string}}) => {
+
+const prisma = new PrismaClient();
+
+const fetchRestaurantByCity = (city: string | undefined) => {
+  if (!city) return prisma.restaurant.findMany();
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city,
+        },
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      price: true,
+      location: true,
+      cuisine: true
+    }
+  });
+};
+
+const Search = async ({ searchParams }: { searchParams: { city: string } }) => {
+  const restaurants = await fetchRestaurantByCity(searchParams.city.toLowerCase());
+
   
   return (
     <>
@@ -10,7 +37,9 @@ const Search = ({searchParams}: {searchParams: {city: string}}) => {
       <div className="flex py-4 m-auto w-2/3 justify-between items-start text-black">
         <SearchSideBar />
         <div className="w-5/6">
-          <SearchRestaurantCard city={searchParams.city}/>
+          {restaurants.map((restaurant) => (
+            <SearchRestaurantCard restaurant={restaurant}/>
+          ))}
         </div>
       </div>
     </>
