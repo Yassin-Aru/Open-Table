b28e29999c0d41f0c9136178af4e251bbe7d279b
Yassin-Aru <jocarhisouka@gmail.com>
2023-08-31 18:44:42 +0100

    FIXING! Unhandled Runtime Error

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,7 +3,6 @@ import Header from "./components/Header";
 import SearchRestaurantCard from "./components/SearchRestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
 
-
 const prisma = new PrismaClient();
 
 const fetchRestaurantByCity = (city: string | undefined) => {
@@ -22,39 +21,44 @@ const fetchRestaurantByCity = (city: string | undefined) => {
       main_image: true,
       price: true,
       location: true,
-      cuisine: true
-    }
+      cuisine: true,
+    },
   });
 };
 
 const fetchLocations = async () => {
   return prisma.location.findMany();
-}
+};
 
 const fetchCuisine = async () => {
   return prisma.cuisine.findMany();
-}
+};
 
 const Search = async ({ searchParams }: { searchParams: { city: string } }) => {
-  const restaurants = await fetchRestaurantByCity(searchParams.city.toLowerCase());
+  const restaurants = await fetchRestaurantByCity(
+    searchParams.city.toLowerCase()
+  );
   const location = await fetchLocations();
   const cuisine = await fetchCuisine();
-    
+
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start text-black">
-        <SearchSideBar locations={location} cuisines={cuisine}/>
+        <SearchSideBar locations={location} cuisines={cuisine} />
         <div className="w-5/6">
           {restaurants.length ? (
             restaurants.map((restaurant) => (
-              
-              <SearchRestaurantCard restaurant={restaurant} key={restaurant.id}/>
+              <SearchRestaurantCard
+                restaurant={restaurant}
+                key={restaurant.id}
+              />
             ))
           ) : (
-            <p className="font-reg text-3xl capitalize text-gray-700">Sorry, There is not restaurants in this area!</p>
-          )
-          }
+            <p className="font-reg text-3xl capitalize text-gray-700">
+              Sorry, There is not restaurants in this area!
+            </p>
+          )}
         </div>
       </div>
     </>
