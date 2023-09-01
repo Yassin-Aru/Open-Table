d12562fa47e040b576aa57e5925fb3eb6d7f0b43
Yassin-Aru <jocarhisouka@gmail.com>
2023-09-01 16:48:42 +0100

    Adding Price params...

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -4,12 +4,13 @@ import Link from "next/link";
 function SearchSideBar({
   locations,
   cuisines,
-  searchParams
+  searchParams,
 }: {
   locations: Location[];
   cuisines: Cuisine[];
-  searchParams: { city: string; cuisine?: string; price?: PRICE }
+  searchParams: { city: string; cuisine?: string; price?: PRICE };
 }) {
+  const prices: string[] = ["$", "$$", "$$$"];
   return (
     <div className="w-1/5">
       <div className="border-b pb-4 flex flex-col">
@@ -52,13 +53,25 @@ function SearchSideBar({
       <div className="mt-3 pb-4">
         <h1 className="mb-2">Price</h1>
         <div className="flex">
-          <button className="border w-full text-reg  rounded-l p-2">$</button>
-          <button className="border-r border-t border-b w-full text-reg  p-2">
-            $$
-          </button>
-          <button className="border-r border-t border-b w-full text-reg  p-2 rounded-r">
-            $$$
-          </button>
+          {prices.map((price) => (
+            <Link
+              href={{
+                pathname: "/search",
+                query: {
+                  ...searchParams,
+                  price:
+                    price === "$"
+                      ? "Cheap"
+                      : price === "$$"
+                      ? "Regular"
+                      : "High",
+                },
+              }}
+              className="border w-full text-reg  rounded-l p-2"
+            >
+              {price}
+            </Link>
+          ))}
         </div>
       </div>
     </div>
